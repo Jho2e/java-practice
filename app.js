f0fa18880b862879020c174cdac4e59881c05ed7
Jho2e <ohjaehokor@naver.com>
2022-03-04 17:07:29 +0900

    v toggle v
    js에서 style을 처리하기보다는
    css를 활용 &
    번거로운 condition말고
    toggle을 이용한 처리

---
+++ app.js
@@ -0,0 +1,7 @@
+const xxxx = document.querySelector("div.yyyy:first-child h1");
+
+function handleTitleClick() {
+  xxxx.classList.toggle("clickedvv");
+}
+
+xxxx.addEventListener("click", handleTitleClick);
