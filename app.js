de09ba050ab040ef6991b795c3f43a90c3d948e2
Jho2e <ohjaehokor@naver.com>
2022-03-07 11:01:10 +0900

    login
    js 연습- 로그인창
    : form 과 h1이 상황에 맞게 등장하거나 사라지는

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username2 = loginInput.value;
+
+  localStorage.setItem(USERNAME_KEY, username2);
+  //  localStorage.setItem("저장할 아이템의 이름", 값 = username 변수);
+
+  /*   greeting.innerText = `로그인 감샤. Hello ${username2} gogo!`;
+  //  greeting.innerText = "Hello " + username2;
+
+  greeting.classList.remove(HIDDEN_CLASSNAME); */
+
+  paintGreetings(username2);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(nameOfUser) {
+  greeting.innerText = `로그인 감샤. Hello ${nameOfUser}. gogo!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  //저장된 유저네임이 없다면 입력창을 보여줘야함 show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // 유저네임이 있다면 form은 없애고 greeting을 보여주면됨
+  paintGreetings(savedUsername);
+}
